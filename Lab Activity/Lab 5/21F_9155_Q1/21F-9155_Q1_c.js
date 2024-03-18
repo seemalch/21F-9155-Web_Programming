9078e3995a58ab7fbbdee2ad053281cfebae670e
ZainMehmood123 <mehmoodzain32@gmail.com>
2024-03-18 19:49:50 +0500

    Add new_folder to project

---
+++ Lab Activity/Lab 5/21F_9155_Q1/21F-9155_Q1_c.js
@@ -0,0 +1,10 @@
+// In JavaScript, you can use
+// for single-line comments or /* */ for multi-line comments.
+// This is a JavaScript comment
+var s = 10;
+
+/*
+This is a multi-line comment.
+It spans multiple lines.
+*/
+var y = "seemal zia";
